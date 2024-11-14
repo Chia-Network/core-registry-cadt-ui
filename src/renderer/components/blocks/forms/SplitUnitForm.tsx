15545be09d4d1543a2ec8600a6895f253a3623cf
William "Zan" Wills <wwills2@protonmail.com>
2024-11-14 16:53:35 -0500

    feat: Full UI refactor
    * feat: configred refactor branch
    
    * feat: repo configurations
    
    * feat: creat org form updates
    feat: styling updates
    feat: minor fixes and improvements
    
    * feat: fixed staging diff modal behavior
    
    * fix: translation token and console.log removal
    
    * feat: removed unitblockstart and unitblockend from create unit form
    feat: unitblockstart and unitblockend readonly in split unit form
    fix: removed issuance timestaged in selected issuance
    
    * fix: split unit form
    fix: Diffviewer single object handling
    
    * fix: hardcoded api host in fila download fetch requests
    
    * feat: query param hook automatically sets query param default value
    
    * feat: child app changes WIP
    
    * feat: updated child app to look in local storage for parent settings
    feat: updated app logic to handle the standalone vs child settings sources
    
    * feat: added parent reload logic
    feat: child save and reload location logic WIP
    
    * feat: child app tracks and reloads its own location when running as a child app
    
    * feat: child app tracks and reloads url hashes
    feat: child app loads theme colors set from parent
    feat: added useManageSavedLocation hook to track app location
    feat: child app will not load saved location if user enters url
    
    * fix: uncommented code
    
    * feat: minor adjustments
    
    * feat: minor adjustments
    
    * feat: adjusted app window size when child app
    
    * feat: update README.md
    
    * build: update package.json names and details
    
    * ci: update build scripts to latest format
    
    * build: update packages
    
    * docs:  README updates for naming
    
    ---------
    
    Co-authored-by: Zachary Brown <z.brown@chia.net>
    Co-authored-by: Zachary Brown <zachary.g.brown@gmail.com>

---
+++ src/renderer/components/blocks/forms/SplitUnitForm.tsx
@@ -0,0 +1,125 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { Form, Formik, FormikProps } from 'formik';
+import { Field, Repeater } from '@/components';
+import * as yup from 'yup';
+import { UnitSplit } from '@/schemas/Unit.schema';
+import { PickList } from '@/schemas/PickList.schema';
+import { deepOmit, validateAndSubmitFieldArrayForm } from '@/utils/formik-utils';
+import { FormattedMessage, useIntl } from 'react-intl';
+import _ from 'lodash';
+
+const validationSchema = yup.object({
+  units: yup.array().of(
+    yup.object({
+      unitCount: yup.number().required('Unit count is required'),
+      unitOwner: yup.string().nullable(),
+      countryJurisdictionOfOwner: yup.string().required('Country jurisdiction of owner is required'),
+      inCountryJurisdictionOfOwner: yup.string().required('In-Country jurisdiction of owner is required'),
+    }),
+  ),
+});
+
+interface SplitUnitFormProps {
+  picklistOptions?: PickList | null;
+  setFormValidationError: (formValid: boolean) => void;
+}
+
+export interface SplitUnitFormRef {
+  submitForm: () => Promise<any>;
+}
+
+const SplitUnitForm = forwardRef<SplitUnitFormRef, SplitUnitFormProps>(
+  ({ picklistOptions, setFormValidationError }, ref) => {
+    const intl = useIntl();
+    const formikRef = useRef<FormikProps<any>>(null);
+
+    useImperativeHandle(ref, () => ({
+      submitForm: async () =>
+        deepOmit(await validateAndSubmitFieldArrayForm(formikRef, 'units'), [
+          'orgUid',
+          'warehouseProjectId',
+          'timeStaged',
+        ]),
+    }));
+
+    return (
+      <Formik
+        innerRef={formikRef}
+        initialValues={{ units: [] }}
+        validationSchema={validationSchema}
+        validateOnChange
+        onSubmit={(values) => console.log(values)}
+      >
+        {() => (
+          <Form onClick={() => setFormValidationError(!_.isEmpty(formikRef?.current?.errors))}>
+            <Repeater<UnitSplit>
+              name="units"
+              maxNumber={5}
+              minNumber={3}
+              readonly={false}
+              initialValue={[
+                {
+                  unitCount: null,
+                  unitOwner: null,
+                  countryJurisdictionOfOwner: null,
+                  inCountryJurisdictionOfOwner: null,
+                },
+                {
+                  unitCount: null,
+                  unitOwner: null,
+                  countryJurisdictionOfOwner: null,
+                  inCountryJurisdictionOfOwner: null,
+                },
+              ]}
+              itemTemplate={{
+                unitCount: 0,
+                unitOwner: null,
+                countryJurisdictionOfOwner: null,
+                inCountryJurisdictionOfOwner: null,
+              }}
+            >
+              {(_, index, name) => (
+                <>
+                  <div>
+                    <div className="font-medium pb-6">
+                      <FormattedMessage id="new-unit" /> {index + 1}
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4">
+                      <Field
+                        name={`${name}[${index}].unitCount`}
+                        required={true}
+                        label={intl.formatMessage({ id: 'unit-count' })}
+                        type="number"
+                      />
+                      <Field
+                        name={`${name}[${index}].unitOwner`}
+                        label={intl.formatMessage({ id: 'unit-owner' })}
+                        type="text"
+                      />
+                      <Field
+                        name={`${name}[${index}].countryJurisdictionOfOwner`}
+                        label={intl.formatMessage({ id: 'country-jurisdiction-of-owner' })}
+                        required={true}
+                        type="picklist"
+                        options={picklistOptions?.countries}
+                      />
+                      <Field
+                        name={`${name}[${index}].inCountryJurisdictionOfOwner`}
+                        label={intl.formatMessage({ id: 'in-country-jurisdiction-of-owner' })}
+                        required={true}
+                        type="picklist"
+                        options={picklistOptions?.countries}
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
+            </Repeater>
+          </Form>
+        )}
+      </Formik>
+    );
+  },
+);
+
+export { SplitUnitForm };
